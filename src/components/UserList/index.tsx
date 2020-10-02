0199b433de62fb191c932fdf60f79377e9ce9b03
Vinicius Benedito <beneditoloura@gmail.com>
2020-10-02 09:31:32 -0300

    Finalização do Componente UserList

--- src/components/UserList/index.tsx
+++ src/components/UserList/index.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Role, User, Avatar, Strong, Span } from './styles';
+
+interface UserProps {
+    nickname: string;
+    isBot?: boolean;
+}
+
+const UserRow: React.FC<UserProps> = ({ nickname, isBot }) => {
+    return (
+        <User>
+            <Avatar className={isBot ? 'bot': ''} />
+            <Strong>{nickname}</Strong>
+
+            {isBot && <Span>Bot</Span>}
+        </User>
+    )
+}
 
 
 const UserList: React.FC = () => {
     return (
         <Container>
-            
+            <Role>Disponível - 1</Role>
+            <UserRow nickname="Guilherme Rodz" />
+
+            <Role>Offline - 18</Role>
+            <UserRow nickname="Diego Fernandes" isBot />
         </Container>
     );
 }
