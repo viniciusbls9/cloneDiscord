af7c81cb27051f49df4c6729d2b4369888047dc5
Vinicius Benedito <beneditoloura@gmail.com>
2020-10-02 09:11:52 -0300

    Inicio do Componente UserList

---
+++ src/components/UserList/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+
+const UserList: React.FC = () => {
+    return (
+        <Container>
+            
+        </Container>
+    );
+}
+
+export default UserList;
