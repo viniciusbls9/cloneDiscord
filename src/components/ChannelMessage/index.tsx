41dad0f833c1d60ea968e133e8c8b7064ebe5ea7
Vinicius Benedito <beneditoloura@gmail.com>
2020-10-03 10:23:12 -0300

    Inicio do componente ChannelData

---
+++ src/components/ChannelMessage/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const ChannelMessage: React.FC = () => {
+    return (
+        <h1>dsds</h1>
+    );
+}
+
+export default ChannelMessage;
